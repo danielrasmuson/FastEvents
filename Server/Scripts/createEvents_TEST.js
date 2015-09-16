922e4f4e21478286880bcf17dcc0ceb2f7b599f9
Daniel Rasmuson <dan123911@gmail.com>
2015-09-16 11:14:52 -0700

    its pasting to my calendar

--- Server/Scripts/createEvents_TEST.js
+++ Server/Scripts/createEvents_TEST.js
@@ -13,22 +13,46 @@ var getAuthFromToken = require('./google/calendar/oauthToken').getAuthFromToken;
 //     expect(auth.credentials.access_token).toEqual('bbbbb');
 //     done();
 //   })
-var TOKEN = "ya29.8AEz5nkn1qYRxdc9zMY7dDvv7xKrr1A-WM00Lo0wBc1OLeLGoEGhLtJq5P_uB-_GtiWfcQ";
+var TOKEN = "ya29.8AGJOxda09p0FfGDZHpmutS_v1cbPTa4BmX7PAs9Fz8OPWjlKxGaVVYhD5nMKKdJ1zMRWQ";
 
-meetup.query({category: "34"}).forEach((events)=>{
+function formatLocalDate(date) {
+  var tzo = -date.getTimezoneOffset(),
+    dif = tzo >= 0 ? '+' : '-',
+    pad = function(num) {
+        var norm = Math.abs(Math.floor(num));
+        return (norm < 10 ? '0' : '') + norm;
+    };
+  return date.getFullYear()
+    + '-' + pad(date.getMonth()+1)
+    + '-' + pad(date.getDate())
+    + 'T' + pad(date.getHours())
+    + ':' + pad(date.getMinutes())
+    + ':' + pad(date.getSeconds())
+    + dif + pad(tzo / 60)
+    + ':' + pad(tzo % 60);
+}
+
+meetup.query({
+    category: "34",
+    lat: "37.775334",
+    lon: "-122.394552",
+    radius: "2"
+    // time: "2w"
+  }).forEach((events)=>{
   // only doing the first 10 events for now
   getAuthFromToken(TOKEN).forEach((auth)=>{
     _.slice(events, 0, 3).forEach((event)=>{
-      console.log(event)
+      console.log(event.group.event_url);
+      console.log(event.group.group_lat);
+      console.log(event.group.group_lon);
+      console.log();
       calendar.createEvent({
         auth: auth,
         start: {
-          'dateTime': '2015-09-15T09:00:00-07:00',
-          'timeZone': 'America/Los_Angeles',
+          'dateTime': formatLocalDate(new Date(event.time)),
         },
         end: {
-          'dateTime': '2015-09-15T17:00:00-07:00',
-          'timeZone': 'America/Los_Angeles',
+          'dateTime': formatLocalDate(new Date(event.time+event.duration)),
         },
         title: event.group.name,
         description: event.description,
@@ -36,7 +60,7 @@ meetup.query({category: "34"}).forEach((events)=>{
           {'email': 'dan123911@gmail.com'}
         ]
       }).forEach(function(success){
-        console.log(success);
+        console.log('event created');
       },(err)=>{
         console.log(err);
       })

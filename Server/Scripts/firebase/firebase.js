4dea0ef8b3113b526936a3be66c17d7181cfc12a
Daniel Rasmuson <dan123911@gmail.com>
2015-09-27 17:28:12 -0700

    upated user object

--- Server/Scripts/firebase/firebase.js
+++ Server/Scripts/firebase/firebase.js
@@ -7,14 +7,35 @@
 
 var request = require('request');
 var DOMAIN = "https://fastevents.firebaseio.com/";
+var Rx = require('rx');
 
 function user(uid){
   return Rx.Observable.create((observer)=>{
     request.get(DOMAIN+ 'users/' + uid + '.json', function(err, response, body){
-      observer.onNext(JSON.parse(body));
+      observer.onNext(new User(JSON.parse(body)));
       observer.onCompleted();
     });
   });
+}
+
+function User(firebaseObj) {
+  this.user = firebaseObj;
+  
+  this.calendarId = function() {
+    return this.user.calendar.google.calendar_id;
+  };
+  this.calendarToken = function() {
+    return this.user.calendar.google.token.access_token;
+  };
+  this.location = function() {
+    return this.user.location;
+  };
+  this.email = function() {
+    return this.user.email;
+  };
+  this.apiFilters = function() {
+    return this.user.event_preferences;
+  }; 
 }
 
 module.exports = {
--- Server/Scripts/firebase/firebase_spec.js
+++ Server/Scripts/firebase/firebase_spec.js
@@ -3,11 +3,15 @@ require('dotenv').load(); // load .env
 
 var USER_ID = '8b7a898d-6085-4fbe-80f9-958ebca0928e'
 
-describe('firebase api', function () {
-  it('should fetch user data', function (done) {
+fdescribe('firebase api', function () {
+  fit('should fetch user data', function (done) {
     firebase.user(USER_ID).forEach(function(userData){
       expect(userData).toBeDefined();
-      expect(userData.email).toEqual('dan123911@gmail.com');
+      expect(userData.calendarId()).toEqual('5hj6m57a6rt2muull7t62dka90');
+      expect(userData.calendarToken()).toEqual('ya29.8AHTs7dtkKuLEWosmf21Bt_Tbvmppc9X97goBbKx63k87bj8wfNQ7tX6eZUDGPIwEXSd');
+      expect(userData.location().address).toEqual('351 King St');
+      expect(userData.email()).toEqual('dan123911@gmail.com');
+      expect(userData.apiFilters().max_miles).toEqual(2);
       done();
     });
   });
